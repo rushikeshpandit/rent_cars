353aaf555fb2a91ca94ffa2f7d807750043b7f95
Rushikesh Pandit <rushikesh.d.pandit@gmail.com>
2023-12-21 20:08:13 +0530

    - Added sobelow dependency

--- mix.exs
+++ mix.exs
@@ -58,10 +58,10 @@ defmodule RentCars.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:plug_cowboy, "~> 2.5"}
       {:plug_cowboy, "~> 2.5"},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:excoveralls, "~> 0.18", only: :test},
+      {:sobelow, "~> 0.13", only: [:dev, :test], runtime: false},
     ]
   end
 
