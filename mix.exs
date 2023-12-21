52cd254d0c54453683783e0bd005c31cc051e9e9
Rushikesh Pandit <rushikesh.d.pandit@gmail.com>
2023-12-21 20:09:01 +0530

    - Added dialyzer dependency

--- mix.exs
+++ mix.exs
@@ -62,6 +62,7 @@ defmodule RentCars.MixProject do
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:excoveralls, "~> 0.18", only: :test},
       {:sobelow, "~> 0.13", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
     ]
   end
 
