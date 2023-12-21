5a0d3c54666299b93aa604e4c0156cd70c80e7c9
Rushikesh Pandit <rushikesh.d.pandit@gmail.com>
2023-12-21 20:06:12 +0530

    - Added excoveralls dependency

--- mix.exs
+++ mix.exs
@@ -9,7 +9,15 @@ defmodule RentCars.MixProject do
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test,
+        "coveralls.cobertura": :test
+      ]
     ]
   end
 
@@ -51,7 +59,9 @@ defmodule RentCars.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:excoveralls, "~> 0.18", only: :test},
     ]
   end
 
