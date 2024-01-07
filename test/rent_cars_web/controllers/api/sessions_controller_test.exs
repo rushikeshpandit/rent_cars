0acb7d703d95d00b3cf01310973c58877810feb2
Rushikesh Pandit <rushikesh.d.pandit@gmail.com>
2024-01-07 18:52:16 +0530

    - Added tests for rest password

--- test/rent_cars_web/controllers/api/sessions_controller_test.exs
+++ test/rent_cars_web/controllers/api/sessions_controller_test.exs
@@ -1,5 +1,6 @@
 defmodule RentCarsWeb.Api.SesionsControllerTest do
   use RentCarsWeb.ConnCase
+  alias RentCars.Shared.Tokenr
 
   describe "handle with session" do
     setup :include_normal_user_token
@@ -32,6 +33,21 @@ defmodule RentCarsWeb.Api.SesionsControllerTest do
         )
 
       assert response(conn, 204) == ""
+    end
+
+    test "reset password", %{conn: conn, user: user} do
+      token = Tokenr.generate_forgot_email_token(user)
+
+      conn =
+        post(
+          conn,
+          Routes.api_session_path(conn, :reset_password,
+            token: token,
+            user: %{password: "Varenya@5199", password_confirmation: "Varenya@5199"}
+          )
+        )
+
+      assert json_response(conn, 200)["data"]["user"]["data"]["email"] == user.email
     end
   end
 end
