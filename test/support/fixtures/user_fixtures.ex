73f91453d06aa944cef349af73a8d9d4c9a242b2
Rushikesh Pandit <rushikesh.d.pandit@gmail.com>
2024-01-09 19:42:20 +0530

    - linting changes

--- test/support/fixtures/user_fixtures.ex
+++ test/support/fixtures/user_fixtures.ex
@@ -2,16 +2,15 @@ defmodule RentCars.UserFixtures do
   alias RentCars.Accounts
 
   def user_attrs(attrs \\ %{}) do
-    valid_attrs =
-      %{
-        first_name: "rushikesh",
-        last_name: "pandit",
-        user_name: "rushikeshpandit#{:rand.uniform(10_000)}",
-        password: "Rushi@7588",
-        password_confirmation: "Rushi@7588",
-        email: "rushikesh.d.pandit#{:rand.uniform(10_000)}@gmail.com",
-        drive_license: "dr #{:rand.uniform(10_000)}"
-      }
+    valid_attrs = %{
+      first_name: "rushikesh",
+      last_name: "pandit",
+      user_name: "rushikeshpandit#{:rand.uniform(10_000)}",
+      password: "Rushi@7588",
+      password_confirmation: "Rushi@7588",
+      email: "rushikesh.d.pandit#{:rand.uniform(10_000)}@gmail.com",
+      drive_license: "dr #{:rand.uniform(10_000)}"
+    }
 
     Enum.into(attrs, valid_attrs)
   end
