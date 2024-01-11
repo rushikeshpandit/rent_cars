9e94ccb9237dd3bab7b18a534db0fd4a23d6ca75
Rushikesh Pandit <13253098+rushikeshpandit@users.noreply.github.com>
2024-01-11 14:34:00 +0530

    2.3.9 check if user booked car (#29)
    * - Added rental fixture
    
    * - Rental schema changes
    
    * - Added method to check if car is booked by user or not
    
    * - Added has_many attribute to user schema
    
    * - Added test for car rental

--- lib/rent_cars/rentals/create_rental.ex
+++ lib/rent_cars/rentals/create_rental.ex
@@ -1,11 +1,14 @@
 defmodule RentCars.Rentals.CreateRental do
-  import RentCars.Shared.DateValidations, only: [check_if_is_more_than_24_hours: 1]
+  import Ecto.Query
+  alias RentCars.Accounts.User
   alias RentCars.Cars.Car
   alias RentCars.Repo
+  import RentCars.Shared.DateValidations, only: [check_if_is_more_than_24_hours: 1]
 
-  def execute(car_id, expected_return_date) do
+  def execute(car_id, expected_return_date, user_id) do
     with true <- check_if_is_more_than_24_hours(expected_return_date),
-         {:ok, car} <- car_available?(car_id) do
+         {:ok, car} <- car_available?(car_id),
+          true <- user_booked_car?(user_id) do
       car
     else
       error -> error
@@ -20,4 +23,16 @@ defmodule RentCars.Rentals.CreateRental do
 
   defp check_car_availability(%{available: false}), do: {:error, "Car is unavailable"}
   defp check_car_availability(car), do: {:ok, car}
+
+  defp user_booked_car?(user_id) do
+    err = {:error, "User has a reservation"}
+
+    User
+    |> join(:inner, [u], r in assoc(u, :rentals))
+    |> where([u, _r], u.id == ^user_id)
+    |> where([_u, r], is_nil(r.end_date))
+    |> select([u, _r], count(u.id))
+    |> Repo.one()
+    |> then(&(&1 == 0 || err))
+  end
 end
---
+++ test/support/fixtures/rentals_fixtures.ex
@@ -0,0 +1,19 @@
+defmodule RentCars.RentalsFixtures do
+  alias RentCars.Rentals.Rental
+  alias RentCars.Repo
+
+  def rental_fixture(attrs \\ %{}) do
+    expected_return_date = then(NaiveDateTime.utc_now(), &%{&1 | month: &1.month + 1})
+
+    attrs =
+      Enum.into(attrs, %{
+        expected_return_date: expected_return_date,
+        start_date: NaiveDateTime.utc_now()
+      })
+
+    changeset = Rental.changeset(%Rental{}, attrs)
+    {:ok, rental} = Repo.insert(changeset)
+
+    rental
+  end
+end
