0da2edddcf6f9a3adc90c4a13a2c4ebb3d759455
Rushikesh Pandit <rushikesh.d.pandit@gmail.com>
2024-01-12 16:54:26 +0530

    - Added api to upload photo

--- lib/rent_cars_web/controllers/api/user_json.ex
+++ lib/rent_cars_web/controllers/api/user_json.ex
@@ -1,4 +1,5 @@
 defmodule RentCarsWeb.Api.UserJSON do
+  alias RentCars.Accounts.Avatar
   alias RentCars.Accounts.User
   # If you want to customize a particular status code,
   # you may add your own clauses, such as:
@@ -23,6 +24,8 @@ defmodule RentCarsWeb.Api.UserJSON do
       user_name: user.user_name,
       email: user.email,
       drive_license: user.drive_license,
+      role: user.role,
+      avatar: Avatar.url({user.avatar, user}),
       inserted_at: user.inserted_at,
       updated_at: user.updated_at
     }
